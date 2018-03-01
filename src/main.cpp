93dca1a8fcc46921579632f1c56b8a319e5a576c
Aidan Hahn <aidanjacobhahn@gmail.com>
2018-02-28 22:30:02 -0800

    dummy output in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main () {
-  std::cout << "this is where program output would be" std::endl;
+  std::cout << "this is where program output would be" << std::endl;
   return 0;
 }
